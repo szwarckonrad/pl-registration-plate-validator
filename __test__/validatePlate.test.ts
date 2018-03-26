5802bf3d1d16f552b5093502241b4591d90a36d5
Konrad Szwarc <k.szwarc@rynekpierwotny.pl>
2018-03-26 10:40:37 +0200

    Workspace setup completed.

---
+++ __test__/validatePlate.test.ts
@@ -0,0 +1,7 @@
+import {validatePlate} from "../lib";
+
+describe("validatePlate module tests", () => {
+    test("INIT TEST | Function is defined", () => {
+        expect(validatePlate).toBeDefined();
+    })
+});
