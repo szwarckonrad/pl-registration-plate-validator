53f91fa5ece6ecc5b7933503bc1af6da3b5efbb4
Konrad Szwarc <k.szwarc@rynekpierwotny.pl>
2018-03-27 11:38:54 +0200

    TEST | Module structure + vanity plate validation.

--- __test__/validatePlate.test.ts
+++ __test__/validatePlate.test.ts
@@ -1,7 +1,19 @@
 import {validatePlate} from "../lib";
 
-describe("validatePlate module tests", () => {
-    test("INIT TEST | Function is defined", () => {
+describe("validatePlate initial tests", () => {
+    test("Function is defined", () => {
         expect(validatePlate).toBeDefined();
-    })
+    });
+    test("Throw error if typeof userInput !== \"string\", pass otherwise", () => {
+        expect(() => validatePlate(0).toThrowError(TypeError));
+    });
+    test("Return false if provided plate first symbol isnt a letter", () => {
+        expect(validatePlate("0WY00")).toBe(false);
+    });
+});
+
+describe("validatePlate return tests", () => {
+    test("Providing valid vanity plate should return gloabl true", () => {
+        expect(validatePlate("W0 TEST")).toBe(true);
+    });
 });
